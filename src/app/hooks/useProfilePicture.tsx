c3af1e054e60c865035ad809067557613922c29a
NainnMaboul <maxence.lombard@supinfo.com>
2025-06-14 14:52:22 +0200

    multiple image upload fix

--- src/app/hooks/useProfilePicture.tsx
+++ src/app/hooks/useProfilePicture.tsx
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store/store.ts";
 import { addAttachment } from "../store/slices/attachmentSlice.ts";
 
+const pendingDownloads: Record<string, Promise<void>> = {};
+
 const useProfilePicture = (profilePictureId: string | undefined) => {
   const dispatch = useDispatch();
   const [userImage, setUserImage] = useState<string | undefined>(undefined);
@@ -15,22 +17,30 @@ const useProfilePicture = (profilePictureId: string | undefined) => {
   const setUserProfilePicture = async () => {
     if (!profilePictureId) return;
     if (profilePictureUrls[profilePictureId]) {
-      setUserImage(profilePictureUrls[profilePictureId]);
       return;
     }
-    const blob = await GetProfilePicture(profilePictureId).unwrap();
-    const url = URL.createObjectURL(blob);
-    dispatch(addAttachment({ id: profilePictureId, url }));
-    setUserImage(url);
+    if (pendingDownloads[profilePictureId] !== undefined) {
+      await pendingDownloads[profilePictureId];
+      return;
+    }
+    pendingDownloads[profilePictureId] = (async () => {
+      const blob = await GetProfilePicture(profilePictureId).unwrap();
+      const url = URL.createObjectURL(blob);
+      dispatch(addAttachment({ id: profilePictureId, url }));
+    })();
+    await pendingDownloads[profilePictureId];
+    delete pendingDownloads[profilePictureId];
   };
 
   useEffect(() => {
     if (!profilePictureId) {
       setUserImage(undefined);
+    } else if (profilePictureUrls[profilePictureId]) {
+      setUserImage(profilePictureUrls[profilePictureId]);
     } else {
       setUserProfilePicture();
     }
-  }, [profilePictureId]);
+  }, [profilePictureId, profilePictureUrls]);
 
   return userImage;
 };
